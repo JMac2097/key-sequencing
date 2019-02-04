4d373792bac1169683f707ec87827af5e115b771
John McKie <john.mckie@remarkable.net>
2019-02-04 16:49:42 +0000

    complete

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
-
+    
     const options = {
-        eventType: 'keydown',
-        keystrokeDelay: 1000
-    };
+        eventType : 'keydown',
+        keyStrokeDelay : 1000
+    }
 
     keyMapper([updateBackground, updateUI], options);
+
 });
 
 function keyMapper(callbackList, options) {
+
     const eventType = options && options.eventType || 'keydown';
-    const keystrokeDelay = options && options.keystrokeDelay || 1000;
+    const keyStrokeDelay = options && options.keyStrokeDelay || 1000;
 
     let state = {
         buffer: [],
         lastKeyTime: Date.now()
     };
-
+    
     document.addEventListener(eventType, event => {
+        
         const key = event.key.toLowerCase();
         const currentTime = Date.now();
         let buffer = [];
-
-        if (currentTime - state.lastKeyTime > keystrokeDelay) {
+        
+        if (currentTime - state.lastKeyTime > keyStrokeDelay) {
             buffer = [key];
         } else {
             buffer = [...state.buffer, key];
@@ -32,25 +35,30 @@ function keyMapper(callbackList, options) {
         state = {buffer: buffer, lastKeyTime: currentTime};
 
         callbackList.forEach(callback => callback(buffer));
+        
     });
 }
 
 function updateBackground(keySequence) {
+
     const container = document.querySelector('#background');
     container.style.backgroundImage = `url(images/${keySequence.join('')}.jpg)`;
+
 }
 
 function updateUI(keySequence) {
     const userInput = keySequence.join('');
     const keySequences = {
-        'idfa': 'All Weapons + Ammo',
-        'idkfa': 'All Weapons + Ammo + Keys',
-        'idbeholds': 'Beserk Pack',
-        'idclev31': 'Bonus Level'
+        'idfa': 'All weapons and ammo',
+        'idkfa': 'All weapons and ammo and keys',
+        'idbeholds': 'Beserk pack',
+        'idclev31': 'Bonus level'
     };
+
     const userInputDisplay = document.querySelector('#user_input');
     userInputDisplay.textContent = userInput;
 
     const cheatMessage = document.querySelector('#cheat_message');
-    cheatMessage.textContent = keySequences[userInput] || 'Nothing';
+    cheatMessage.textContent = keySequences[userInput] || 'nothing';
 }
+
